ffaaee40c61fdcab3e5efea5684710e9f1216242
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-22 00:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //your JS code here. If required.
 function test(){
-	let div=document.getElementById("searchdiv");
-	// alert(div)
-	
+	let div=document.getElementsByClassName("search")[0];	
 	div.style.background = "blue";
-	div.focus();
-	document.getElementByClass("btn")[0].focus();
+	div.classList.add("active")
+	document.getElementsByClassName("input")[0].focus();
 }
+
+//your JS code here. If required.
+
+// let box=document.querySelector("div");
+// let input=document.querySelector(".input");
+// let button=document.querySelector(".btn");
+
+// button.addEventListener("click",Onclick);
+// function Onclick(){
+// 	box.classList.add("active");
+// 	// JavaScript code
+// input.focus();
+
+// }
